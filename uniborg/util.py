7471b6386d44983a13139761512345a0aa1fea55
SnapDragon <maliktahir0702@gmail.com>
2019-09-08 15:22:09 +0200

    Update util.py

--- uniborg/util.py
+++ uniborg/util.py
@@ -19,8 +19,7 @@ else:
         from config import Development as Config
 
 
-def admin_cmd(pattern=None, **args):
-    allow_sudo = args.get("allow_sudo", False)
+def admin_cmd(pattern=None, allow_sudo=False **args):
     allow_sudo = True
 
     # get the pattern from the decorator
