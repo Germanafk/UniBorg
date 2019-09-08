b83dbb0df75b52577843751e95692fd03fc6707c
SnapDragon <maliktahir0702@gmail.com>
2019-09-08 15:11:53 +0200

    Update util.py

--- uniborg/util.py
+++ uniborg/util.py
@@ -20,7 +20,7 @@ else:
 
 
 def admin_cmd(pattern=None, **args):
-    allow_sudo = args.get("allow_sudo", True)
+    allow_sudo = args["allow_sudo"]
 
     # get the pattern from the decorator
     if pattern is not None:
