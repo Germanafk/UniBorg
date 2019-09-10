8ed78cd6cef381d751e08b6eb41fb04ab60a6cce
SnapDragon <maliktahir0702@gmail.com>
2019-09-10 21:26:38 +0200

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -55,10 +55,10 @@ async def _(event):
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"{userid}'s @AnimatedStickersGroup"
-        if userid is not 719877937:
-            packshortname = f"Uni_Borg_{userid}_as" # format: Uni_Borg_userid
-        else:
+        if userid == 719877937:
             packshortname = "SnapDragon_Animated"
+        else:
+            packshortname = f"Uni_Borg_{userid}_as" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
