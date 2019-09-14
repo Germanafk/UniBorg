47465614dac8d714a3c898f50ac9c22134e382f8
SnapDragon <maliktahir0702@gmail.com>
2019-09-14 17:18:50 +0200

    Update welcome.py

--- dbplugins/welcome.py
+++ dbplugins/welcome.py
@@ -10,9 +10,6 @@ from sql_helpers.welcome_sql import get_current_welcome_settings, \
     add_welcome_setting, rm_welcome_setting, update_previous_welcome
 from uniborg.util import admin_cmd
 
-TYPE_TEXT = 0
-TYPE_PHOTO = 1
-TYPE_DOCUMENT = 2
 
 @borg.on(events.ChatAction())  # pylint:disable=E0602
 async def _(event):
@@ -26,7 +23,7 @@ async def _(event):
         if event.user_joined or event.user_added:
             if cws.should_clean_welcome:
                 try:
-                    await borg.delete_messages(  # pylint:disable=E0602
+                    await event.client.delete_messages(
                         event.chat_id,
                         cws.previous_welcome
                     )
@@ -36,25 +33,14 @@ async def _(event):
             chat = await event.get_chat()
 
             title = chat.title if chat.title else "this chat"
-            file_media = None
-            if cws.message_type == TYPE_PHOTO:
-                file_media = types.InputPhoto(
-                    int(cws.media_id),
-                    int(cws.media_access_hash),
-                    cws.media_file_reference
-                )
-            elif cws.message_type == TYPE_DOCUMENT:
-                file_media = types.InputDocument(
-                    int(cws.media_id),
-                    int(cws.media_access_hash),
-                    cws.media_file_reference
-                )
-            else:
-                file_media = None
-            #
             participants = await event.client.get_participants(chat)
             count = len(participants)
-            current_saved_welcome_message = cws.custom_welcome_message
+            msg_o = await event.client.get_messages(
+                entity=Config.PRIVATE_CHANNEL_BOT_API_ID,
+                ids=cws.f_mesg_id
+            )
+            current_saved_welcome_message = msg_o.message
+            file_media = msg_o.media
             mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
             first = a_user.first_name
             last = a_user.last_name
@@ -79,31 +65,24 @@ async def _(event):
     if event.fwd_from:
         return
     msg = await event.get_reply_message()
-    if msg and msg.media:
+    if msg:
         if get_current_welcome_settings(event.chat_id):
-            rm_welcome_setting(event.chat_id)
-            media = None
-            message_type = TYPE_TEXT
-            if isinstance(msg.media, types.MessageMediaPhoto):
-                media = utils.get_input_photo(msg.media.photo)
-                message_type = TYPE_PHOTO
-            elif isinstance(msg.media, types.MessageMediaDocument):
-                media = utils.get_input_document(msg.media.document)
-                message_type = TYPE_DOCUMENT
-        #
-            add_welcome_setting(event.chat_id, msg.message, True, 0, message_type, media.id, media.access_hash, media.file_reference)
+            msg_o = await event.client.forward_messages(
+                entity=Config.PRIVATE_CHANNEL_BOT_API_ID,
+                messages=msg,
+                from_peer=event.chat_id,
+                silent=True
+            )
+            add_welcome_setting(event.chat_id, True, 0, msg_o.id)
             await event.edit("Welcome Message updated. ")
         else:
-            media = None
-            message_type = TYPE_TEXT
-            if isinstance(msg.media, types.MessageMediaPhoto):
-                media = utils.get_input_photo(msg.media.photo)
-                message_type = TYPE_PHOTO
-            elif isinstance(msg.media, types.MessageMediaDocument):
-                media = utils.get_input_document(msg.media.document)
-                message_type = TYPE_DOCUMENT
-        #
-            add_welcome_setting(event.chat_id, msg.message, True, 0, message_type, media.id, media.access_hash, media.file_reference)
+            msg_o = await event.client.forward_messages(
+                entity=Config.PRIVATE_CHANNEL_BOT_API_ID,
+                messages=msg,
+                from_peer=event.chat_id,
+                silent=True
+            )
+            add_welcome_setting(event.chat_id, True, 0, msg_o.id)
             await event.edit("Welcome Message saved. ")
     else:
         input_str = event.text.split(None, 1)
@@ -124,7 +103,9 @@ async def _(event):
     rm_welcome_setting(event.chat_id)
     await event.edit(
         "Welcome Message cleared. " + \
-        "The previous welcome message was `{}`.".format(cws.custom_welcome_message)
+        "[This](https://t.me/c/{}/{}) was your previous welcome message.".format(
+            Config.PRIVATE_CHANNEL_BOT_API_ID[4:],
+            cws.f_mesg_id
     )
     
     
@@ -136,7 +117,9 @@ async def _(event):
     if hasattr(cws, 'custom_welcome_message'):
         await event.edit(
             "Welcome Message found.\n " + \
-            "Your Welcome Message is as follows:\n `{}`.".format(cws.custom_welcome_message)
+            "[This](https://t.me/c/{}/{}) is your welcome message.".format(
+            Config.PRIVATE_CHANNEL_BOT_API_ID[4:],
+            cws.f_mesg_id
         )
         return
     else:
