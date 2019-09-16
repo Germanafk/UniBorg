df6707b96c925e30018cf230aece98356ae507fe
SnapDragon <maliktahir0702@gmail.com>
2019-09-16 18:52:44 +0200

    Create tcoin.py

---
+++ stdplugins/tcoin.py
@@ -0,0 +1,15 @@
+from telethon import events
+from uniborg.util import admin_cmd
+
+
+async def tcoing(event):
+    tcoin = event.get_entity("@counting123")
+    @borg.on(events.NewMessage(chats=tcoin, incoming=True))
+    async def _(event):
+        m = int(event.raw_text) + 1
+        event.client.send_message(
+            tcoin.id,
+            m,
+            reply_to=event.message.id
+        )
+            
